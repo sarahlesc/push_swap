b3a91ec5035e09c27425859f5d652f7400eddff0
Sarah LESCURE <slescure@e2r11p20.42.fr>
2021-05-25 11:34:00 +0200

    quartile function

--- sorting/big_sorting.c
+++ sorting/big_sorting.c
@@ -6,10 +6,17 @@ void	large_sorting(t_double_list **stack_a, t_double_list **stack_b,
 	t_double_list	*temp;
 	t_double_list	*cpy;
 	int				median;
+	int				quartile_one;
+	int				quartile_three;
 
 	temp = *stack_a;
 	cpy = ft_double_lstcopy(temp);
 	median = find_median(cpy);
+	quartile_one = find_quartiles(1, cpy);
+	quartile_three = find_quartiles(3, cpy);
+	printf("quartile one = %i\n", quartile_one);
+	printf("quartile three = %i\n", quartile_three);
+	exit (0);
 	while (temp != NULL)
 	{
 		if (temp->content <= median)
@@ -46,6 +53,25 @@ int		find_median(t_double_list *stack)
 		i++;
 	}
 	printf("median = %i\n", temp->content);
+	return (temp->content);
+}
+
+int		find_quartiles(double q, t_double_list *stack)
+{
+	int				i;
+	double			quartile;
+	t_double_list	*temp;
+
+	i = 0;
+	temp = ft_double_lstfirst(stack);
+	quartile = ft_double_lstsize(stack) * (q / 4);
+	swap_sort(&temp);
+	while (i < quartile)
+	{
+		temp = temp->next;
+		i++;
+	}
+	printf("quartile = %i\n", temp->content);
 	return (temp->content);
 }
 
